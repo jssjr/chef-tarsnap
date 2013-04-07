5bf8e6b0c54fe9f324ffc3023f5849f6248d11c1
Scott Sanders <scott@jssjr.com>
2013-04-07 03:58:18 -0400

    Update license headers

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,20 @@
+# Author:: Scott Sanders (ssanders@taximagic.com)
+# Copyright:: Copyright (c) 2013 RideCharge, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 # source install options
 default['tarsnap']['version'] = "1.0.33"
 default['tarsnap']['sha256'] = "0c0d825a8c9695fc8d44c5d8c3cd17299c248377c9c7b91fdb49d73e54ae0b7d"

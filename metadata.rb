82936c7ef5aa5da704be82a502a99db954b3d747
Scott Sanders <scott@jssjr.com>
2013-04-06 22:28:24 -0400

    Typeo in the metadata file

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
 %w{freebsd ubuntu debian}.each do |os|
-  support os
+  supports os
 end
 
 recipe "default", "Installs and configures tarsnap"
